7129ab1789c19fa4f6df56a605c51296c9c60e42
Zinan100 <97530806+Zinan100@users.noreply.github.com>
2022-01-27 18:22:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,25 @@ Rexer=Client(
     api_hash="5023c40ea655bc2834e48888b17ccee8"
 )
 
+from pyrogram import Client, filters
+
+
+
+Rexer=Client(
+    "Pyrogram Bot",
+    bot_token="5015801326:AAEAaUFxm0PFl9fbPoYNH_e6VHMJhvdb4lA",
+    api_id="1306",
+    api_hash="5023c40ea655bc2834e48888b1ee8"
+)
+
 @Rexer.on_message(filters.command("start"))
-async def start_message(bot, message):
-    await message.reply_text("hi i am pyrogram bot on work come after  week later")
+async def
+    start_message(bot, message): await message.reply_text("hi bro")
+
+
+@Rexer.on_message(filters.command("help"))
+async def
+    start_message(bot, message): await message.reply_text("bot set cheyyatte")
 
 
 Rexer.run()
